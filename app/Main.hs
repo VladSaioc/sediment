2c209a16cdd71fef70637b4facb71064a6a3e8aa
Vlad Saioc <vladek244@gmail.com>
2020-05-06 21:57:51 +0200

    Source program read from file, as opposed to console.

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,19 @@
 module Main where
 
+import System.IO
+import Control.Monad
 import GetAst
 import System.Environment
+import Text.Pretty.Simple (pPrint)
 
 main :: IO ()
 main = do
-  s <- getLine
-  let ast = getAst s
-  print ast
+  args <- getArgs
+  let fileName = head args
+  -- if not (null fileName)
+  --   then do source <- readFile fileName
+  --   else do  print "No file name provided. Type a text in Sediment to interpret ad-hoc:"
+  --     source <- getContents
+  source <- readFile fileName
+  let ast = getAst source
+  pPrint ast
