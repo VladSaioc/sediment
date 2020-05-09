cc82979d783349f4a90fc46aaeb37eade57d6f63
Vlad Saioc <vladek244@gmail.com>
2020-05-09 17:48:39 +0200

    Erroneous specification suite for domains.

---
+++ test-errors.sh
@@ -0,0 +1,4 @@
+# Erroneous domain tests
+stack exec Sediment-exe Specifications/Erroneous/Domains-1
+stack exec Sediment-exe Specifications/Erroneous/Domains-2
+stack exec Sediment-exe Specifications/Erroneous/Domains-3
